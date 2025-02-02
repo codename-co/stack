802b304df50c9bd0d4555f2617f6f01f0e78bcdc
Arnaud Leymet <arnaud.leymet@gmail.com>
2025-02-02 19:58:21 +0100

    :wrench: hub: script update

--- packages/website/scripts/update.sh
+++ packages/website/scripts/update.sh
@@ -18,6 +18,13 @@ get_repo_version() {
   gh repo view $repo --json latestRelease | jq -r '.latestRelease.tagName' | sed 's/v//' | sed 's/release-//'
 }
 
+update_stars() {
+  file=$1
+  name=$2
+  stars=$3
+  sed -i '' "s/^stars: .*/stars: $stars/" $file
+}
+
 # get_latest_repo_info "https://github.com/langgenius/dify"
 
 # for each stack in the hub/* directories, read and parse their stack.yaml and display the name and version
@@ -25,9 +32,17 @@ for stack in hub/*/stack.yaml; do
   name=$(grep '^name:' $stack | awk -F': ' '{print $2}')
   version=$(grep '^version:' $stack | awk -F': ' '{print $2}')
   repository=$(grep '^repository:' $stack | awk -F': ' '{print $2}')
+  current_stars=$(grep '^stars:' $stack | awk -F': ' '{print $2}')
   latest_version=$(get_repo_version $repository)
+  stars=$(get_latest_repo_info $repository | jq -r '.stargazerCount')
   if [ "$version" != "$latest_version" ]; then
-    echo "Update $name from $version to $latest_version"
+    echo "⬆️ $name · update from $version to $latest_version"
+  fi
+  if [ "$current_stars" != "$stars" ]; then
+    if [ "$stars" -gt "$current_stars" ]; then
+      echo "⭐$name · stars from $current_stars to $stars"
+      update_stars $stack $name $stars
+    fi
   fi
   # echo "$name: $version ($repository)"
   # echo $(get_repo_version $repository)
