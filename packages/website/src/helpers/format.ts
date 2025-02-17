b1045fd26da1d835c68054a8b38f8b2dcfb00b7e
Arnaud Leymet <arnaud.leymet@gmail.com>
2025-02-17 23:38:52 +0100

    :wrench: Adjustments

--- packages/website/src/helpers/format.ts
+++ packages/website/src/helpers/format.ts
@@ -1,50 +1,31 @@
-const _f =
-  (fractionDigits: number = 1) =>
-  (n: number) =>
-    n.toFixed(fractionDigits).replace(/\.0$/, "");
-
-export const formatSize = (size?: number) => {
+export const formatSize = (size?: number, lang?: string) => {
   if (!size) {
     return "Unknown";
   }
 
-  const f = _f(2);
+  return Intl.NumberFormat(lang, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    notation: "compact",
+    trailingZeroDisplay: "stripIfInteger",
 
-  if (size < 1024) {
-    return size + " B";
-  }
-  const kbs = size / 1024;
-  if (kbs < 1024) {
-    return f(2) + " KB";
-  }
-  const mbs = kbs / 1024;
-  if (mbs < 1024) {
-    return f(2) + " MB";
-  }
-  const gbs = mbs / 1024;
-  return f(2) + " GB";
+    style: "unit",
+    unit: "byte",
+    unitDisplay: "narrow",
+  }).format(size);
 };
 
-export const formatStars = (stars?: number) => {
+export const formatStars = (stars?: number, lang?: string) => {
   if (!stars) {
     return "Unknown";
   }
 
-  const f = _f(1);
-
-  if (stars < 1000) {
-    return stars;
-  }
-  const k = stars / 1000;
-  if (k < 1000) {
-    return f(k) + "k";
-  }
-  const m = k / 1000;
-  if (m < 1000) {
-    return f(m) + "m";
-  }
-  const b = m / 1000;
-  return f(b) + "b";
+  return Intl.NumberFormat(lang, {
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 1,
+    notation: "compact",
+    trailingZeroDisplay: "stripIfInteger",
+  }).format(stars);
 };
 
 import { format as timeagojs, register } from "timeago.js";
