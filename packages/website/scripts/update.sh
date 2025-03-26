67b1f8bdb2a743b495640ad8bf9ff468d8c74202
Arnaud Leymet <arnaud.leymet@gmail.com>
2025-03-26 22:47:16 +0100

    :wrench: Adjustments

--- packages/website/scripts/update.sh
+++ packages/website/scripts/update.sh
@@ -52,6 +52,27 @@ update_stars() {
   sed -i '' "s/^stars: .*/stars: $stars/" $file
 }
 
+update_version() {
+  file=$1
+  version=$2
+  # remove leading "v" or "version-" from version
+  version=$(echo $version | sed 's/^version-//' | sed 's/^v//')
+  sed -i '' "s/^version: .*/version: \"$version\"/" $file
+
+  # update the updated_at date timestamp
+  sed -i '' "s/^updated_at: .*/updated_at: $(date -u +"%Y-%m-%d")/" $file
+
+  # update the version in the compose file
+  # ie: before: image: freshrss/freshrss:${FRESHRSS_VERSION:-1.26.0}
+  #     after:  image: freshrss/freshrss:${FRESHRSS_VERSION:-1.26.1}
+  compose_file=$(echo $file | sed 's/stack.yaml/compose.yaml/')
+  # Extract the prefix before _VERSION and preserve it
+  prefix=$(grep -o "\${[A-Za-z_]*_VERSION:-[0-9.]*}" $compose_file | sed 's/_VERSION:-[0-9.]*}$//')
+  if [ -n "$prefix" ]; then
+    sed -i '' "s/\${[A-Za-z_]*_VERSION:-[0-9.]*}/${prefix}_VERSION:-$version}/" $compose_file
+  fi
+}
+
 # get_latest_repo_info "https://github.com/langgenius/dify"
 
 # for each stack in the hub/* directories, read and parse their stack.yaml and display the name and version
@@ -60,15 +81,19 @@ process_stack() {
   local stack=$1
   local slug=$(grep '^slug:' $stack | awk -F': ' '{print $2}')
   local version=$(grep '^version:' $stack | awk -F': ' '{print $2}')
+  # remove "" from version
+  version=$(echo $version | tr -d '"')
   local repository=$(grep '^repository:' $stack | awk -F': ' '{print $2}')
   local current_stars=$(grep '^stars:' $stack | awk -F': ' '{print $2}')
   local latest_version=$(get_repo_version $repository)
   local stars=$(get_latest_repo_info $repository | jq -r '.stargazerCount')
 
   if [ "$mode" == "version" ]; then
-    if [ "$version" != "$latest_version" ]; then
-      echo "⬆️ $slug · update from $version to $latest_version"
-      # TODO:
+    if [ "$latest_version" != "null" ]; then
+      if [ "$version" != "$latest_version" ]; then
+        echo "⬆️ $slug · update from $version to $latest_version"
+        update_version $stack $latest_version
+      fi
     fi
   fi
 
@@ -93,11 +118,9 @@ for stack in hub/*/stack.yaml; do
   # Limit the number of concurrent jobs
   ((count=count+1))
   if [ $count -ge $MAX_JOBS ]; then
-    wait -n  # Wait for any child to finish
     ((count=count-1))
   fi
 done
 
 # Wait for remaining jobs to finish
 wait
-done
